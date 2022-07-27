226741165bfb4b9f2fbd65dcb392dcfe7f2985b6
Pavel Astakhov <pastakhov@yandex.ru>
2022-07-27 09:09:29 +0600

    Add title text as $1 parameter to accesscontrol-info-box message (#3)

--- src/AccessControlHooks.php
+++ src/AccessControlHooks.php
@@ -145,7 +145,7 @@ class AccessControlHooks {
 		$status = self::canUserDoAction( $user, $tagContentArray, $actionName );
 		if ( !$status->getValue() ) {
 			// User has no access
-			$parserOutput->setText( wfMessage( 'accesscontrol-info-box' )->text() );
+			$parserOutput->setText( wfMessage( 'accesscontrol-info-box', $parserOutput->getTitleText() )->parse() );
 		}
 		if ( !$status->isGood() ) {
 			$text = $parserOutput->getRawText();
@@ -353,7 +353,7 @@ class AccessControlHooks {
 		$tagContentArray = self::getRestrictionForTitle( $title, $user );
 		$isAllowed = self::canUserDoAction( $user, $tagContentArray, $action )->getValue();
 		if ( !$isAllowed ) {
-			$result = 'accesscontrol-info-box';
+			$result = [ 'accesscontrol-info-box', $title->getFullText() ];
 		}
 
 		return $isAllowed;
